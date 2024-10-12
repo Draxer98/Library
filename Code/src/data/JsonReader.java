3b9bf9386f2b6bd8a410c7d0c93c1a90d84f1e59
ErPollo07 <nicola.tosatti@iisviolamarchesini.edu.it>
2024-10-12 15:31:00 +0200

    CREATE data package ; CREATE files package inside data ; CREATE JsonReader a JsonWriter classes

---
+++ Code/src/data/JsonReader.java
@@ -0,0 +1,11 @@
+package data;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+public class JsonReader {
+
+    public static JSONObject readObj(String path) { return new JSONObject(); }
+
+    public static JSONArray readArray(String path) { return new JSONArray(); }
+}
---
+++ Code/src/data/JsonWriter.java
@@ -0,0 +1,11 @@
+package data;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+public class JsonWriter {
+
+    public static void writeObj(JSONObject obj, String path) {}
+
+    public static void writeList(JSONArray arr, String path) {}
+}
