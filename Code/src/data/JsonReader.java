7dd359c6f3c0d67bd7d1a67087d875f3661f3de7
ErPollo07 <nicola.tosatti@iisviolamarchesini.edu.it>
2024-11-03 12:20:55 +0100

    ADD read methods for json file in JsonReader

--- Code/src/data/JsonReader.java
+++ Code/src/data/JsonReader.java
@@ -2,10 +2,45 @@ package data;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.FileReader;
+import java.io.IOException;
 
 public class JsonReader {
 
-    public static JSONObject readObj(String path) { return new JSONObject(); }
+    /**
+     * Reads a JSON file from the specified path and returns a JSONObject.
+     *
+     * @param path the path of the JSON file to read.
+     * @return a JSONObject containing the data from the JSON file.
+     * @throws IOException if an error occurs while reading the file.
+     * @throws ParseException if the file is not in a valid JSON format.
+     */
+    public static JSONObject readObj(String path) throws IOException, ParseException {
+        JSONParser parser = new JSONParser();
 
-    public static JSONArray readArray(String path) { return new JSONArray(); }
+        try (FileReader reader = new FileReader(path)) {
+            // Parse the JSON file and return it as a JSONObject
+            return (JSONObject) parser.parse(reader);
+        }
+    }
+
+    /**
+     * Reads a JSON file from the specified path and returns a JSONArray.
+     *
+     * @param path the path of the JSON file to read.
+     * @return a JSONArray containing the data from the JSON file.
+     * @throws IOException if an error occurs while reading the file.
+     * @throws ParseException if the file is not in a valid JSON format.
+     */
+    public static JSONArray readArray(String path) throws IOException, ParseException {
+        JSONParser parser = new JSONParser();
+
+        try (FileReader reader = new FileReader(path)) {
+            // Parse the JSON file and return it as a JSONArray
+            return (JSONArray) parser.parse(reader);
+        }
+    }
 }
